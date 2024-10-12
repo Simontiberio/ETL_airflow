30862917e83b4418fe279450e1f893d0f6a2af65
Simontiberio <tiberiosimon@gmail.com>
2024-10-11 23:08:17 -0300

    update functions etl

--- functions_etl/update_stock.py
+++ functions_etl/update_stock.py
@@ -2,44 +2,46 @@ import pandas as pd
 from pathlib import Path
 
 def update_stock(stock_file: str, compras_file: str, ventas_file: str, fecha: str):
-    # Directorio donde se encuentran los archivos
+    # Define directory of files.
     base_dir = './base_datos/inventario'
     base_path = Path(base_dir)
     
-    # Cargar los archivos
+    # Load files.
     stock_path = base_path / stock_file
     compras_path = base_path / compras_file
     ventas_path = base_path / ventas_file
     
     if stock_path.exists() and compras_path.exists() and ventas_path.exists():
-        # Leer archivos de stock, compras y ventas
+        # Rad stock, purchases and sells files.
         df_stock = pd.read_excel(stock_path, engine='openpyxl')
         df_compras = pd.read_excel(compras_path, engine='openpyxl')
         df_ventas = pd.read_excel(ventas_path, engine='openpyxl')
-
-        # Convertir columnas de fechas a formato datetime, para luego igualar los formatos
+        
+        
+       # Convert date columns to datetime format, then match the formats.
         df_stock.columns = pd.to_datetime(df_stock.columns, errors='ignore', dayfirst=True)
         df_compras.columns = pd.to_datetime(df_compras.columns, errors='ignore', dayfirst=True)
         df_ventas.columns = pd.to_datetime(df_ventas.columns, errors='ignore', dayfirst=True)
         
-        # Convertir la fecha dada a formato datetime
+        # Convert date ti datetime format.
         fecha = pd.to_datetime(fecha, format='%Y-%m-%d')
 
         
-        # Verificar si la fecha está en los datos
+        # Check if a date is in a columns of DF.
         if fecha not in df_stock.columns or fecha not in df_compras.columns or fecha not in df_ventas.columns:
             print(f"La fecha {fecha} no está presente en los archivos.{df_stock.columns}")
             return
         
-        # Obtener la columna del día anterior al último
-        #fecha_anterior = df_stock.columns[-1]
+        # Get the column from the day before the last one, this case would be viable if the update of the files we use is done automatically. 
+        # As in real life.
+        #previous_date = df_stock.columns[-1]
         
-        # Actualizar stock: stock día anterior + compras - ventas
+        # Update stock = stock of day + units purchases - units sells.
         df_stock[fecha] = df_stock[fecha] + df_compras[fecha] - df_ventas[fecha]
         
-        # Guardar el archivo de stock actualizado
+        # Save a updated file.
         df_stock.to_excel(stock_path, index=False)
         
-        print(f"El archivo de stock ha sido actualizado para la fecha {fecha}. and ")
+        print(f"Stock file has been updated for date {date}.")
     else:
-        print("Uno o más archivos no existen. Verifica los nombres y las rutas.")
+        print("One or more files do not exist. Please check the names and paths.")
