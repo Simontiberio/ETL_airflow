5f2b79b22995a306a03110bc6121181f41933a64
Simontiberio <tiberiosimon@gmail.com>
2024-10-13 22:08:39 -0300

    Define DAG and update task and dependencies task

--- dags/object_dag.py
+++ dags/object_dag.py
@@ -1,8 +1,13 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 from utils.config import api_key, compras_stock,data_quotes,stock_ferrimac,ventas_unidades, monetized_stock, REDSHIFT_SCHEMA
-from functions_etl.obtain_currency import append_to_data_price,obtain_currency
+from functions_etl.obtain_currency import append_to_data_price
 from functions_etl.extract_file_purchases_to_stock import extract_file_purchases_to_stock
 from functions_etl.extract_file_sells_to_stock import extract_file_sells_to_stock
 from functions_etl.update_stock import update_stock
@@ -20,7 +25,7 @@ with DAG (
         'retries': 1,
     },
     description='ETL pipeline to extract,transform and load data into redshift',
-    schedule_interval= 'Daily',
+    schedule_interval= '@daily',
     start_date= datetime(2024,1,1),
     catchup= False,
 ) as dag: 
@@ -65,7 +70,7 @@ with DAG (
     # Task 5 : Transform data, monetize stock using list of prices and data quotes.
 
     transform_data_monetize_stock = PythonOperator( 
-        task_id = 'monetize_stock',
+        task_id = 'monetized_stock',
         python_callable = monetize_stock,
         op_kwargs= {'file' : stock_ferrimac,
                     'file' : data_quotes,
@@ -76,7 +81,7 @@ with DAG (
     # Task 6 : Load data into Redshift.
 
     load_data_monetize_stock = PythonOperator( 
-        task_id = 'monetize_stock',
+        task_id = 'load_data_into_Redshif',
         python_callable = load_data_to_Redshift,
         op_kwargs= {'file' : monetized_stock,
                     'redshift_table' : 'monetized_stock'
@@ -85,4 +90,4 @@ with DAG (
 
     # Set task dependencies.
 
-    [extract_task_API,extract_data_sells_directory,extract_data_purchases_directory] >> transform_data_update_stock >> transform_data_monetize_stock >> transform_data_monetize_stock
+    [extract_task_API,extract_data_sells_directory,extract_data_purchases_directory] >> transform_data_update_stock >> transform_data_monetize_stock >> load_data_monetize_stock
