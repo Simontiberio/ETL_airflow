89130c452a624a8c39889af5a7604560c3caaa83
Simontiberio <tiberiosimon@gmail.com>
2024-10-15 20:46:17 -0300

    create units test

---
+++ test/test_obtain_currency.py
@@ -0,0 +1,26 @@
+import pytest
+import requests
+import pandas as pd
+from unittest.mock import patch
+from functions_etl.obtain_currency import obtain_currency, append_to_data_price
+from utils.config import api_key
+
+
+
+# Mock simulates the result obtained from the API
+
+mock_response_api = {
+    "rates": {"ARS": 989},
+    "date": "2024-10-15"
+}
+
+@patch('requests.get')
+def test_obtain_currency(mock_get):
+
+
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = mock_response_api
+
+    result = obtain_currency(api_key)
+
+    assert result == {'date': '2024-10-15', 'price': 989}, "Result not expected"
