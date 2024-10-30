3ace8d346e64a06eb8294397b4177fb441868776
Simontiberio <tiberiosimon@gmail.com>
2024-10-30 02:14:49 -0300

    update units test

--- test/test_obtain_currency.py
+++ test/test_obtain_currency.py
@@ -4,13 +4,14 @@ import pandas as pd
 from unittest.mock import patch
 from functions_etl.obtain_currency import obtain_currency, append_to_data_price
 from utils.config import api_key
+from datetime import datetime
 
 
 
 # Unit test validates if the dictionary with the expected fields is obtained.
 
 mock_response_api = {
-    "rates": {"ARS": 989},
+    "quotes": {"USDARS": 989},
     "date": "2024-10-15"
 }
 
@@ -22,6 +23,8 @@ def test_obtain_currency(mock_get):
     mock_get.return_value.json.return_value = mock_response_api
 
     result = obtain_currency(api_key)
-    expected = {'date': '2024-10-15', 'price': 989}
+
+    expected_date = datetime.now().strftime('%Y-%m-%d')
+    expected = {'date': expected_date, 'price': 989}
 
     assert result == expected , "Result not expected"
