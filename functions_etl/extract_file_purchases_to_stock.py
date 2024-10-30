d590547182aad2c4d8ffa94aac2b3b808d492cae
Simontiberio <tiberiosimon@gmail.com>
2024-10-30 01:28:19 -0300

    update docstrings

--- functions_etl/extract_file_purchases_to_stock.py
+++ functions_etl/extract_file_purchases_to_stock.py
@@ -3,11 +3,26 @@ import shutil
 from datetime import datetime
 from pathlib import Path
 from utils.config import compras_file
+import logging
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def extract_file_purchases_to_stock( file_compras : str ):
 
 
-    '''Extract and move the purchases file to the inventory directory'''
+    """
+    Extracts and moves the specified purchases file to the inventory directory.
+    
+    Args:
+        file_compras (str): Name of the purchases file to move.
+        
+    Logs:
+        - Errors if the source or destination directories do not exist.
+        - Information confirming the successful move of the file.
+        - Warnings if the specified file does not exist in the source directory.
+    """
 
 
 
@@ -21,11 +36,13 @@ def extract_file_purchases_to_stock( file_compras : str ):
 
     # Verified if exist origin and destination paths.
     if not origin_path.exists():
-        return print(f"Directory  {origin_path} not found.")
-    if not destination_path.exists():
+        logging.error(f"Directory {origin_path} not found.")
+        return
         
-        return print(f"Directory  {destination_path} not found.")
-
+    if not destination_path.exists():
+        logging.error(f"Destination directory {destination_path} not found.")
+        return
+        
     # Origin path define complete.
     archivo_origen = origin_path.joinpath(file_compras)
     
@@ -36,7 +53,9 @@ def extract_file_purchases_to_stock( file_compras : str ):
         # Copy origin file to destination.
         
         shutil.copy(str(archivo_origen), str(archivo_destino))
-        print(f"File '{file_compras}' moved to stock folder correctly.")
+        logging.info(f"File '{file_compras}' moved to stock folder successfully.")
+        
     else:
-        print(f"The file '{file_compras}' was not found in the purchases directory.")
+        logging.warning(f"The file '{file_compras}' was not found in the purchases directory.")
+        
 
